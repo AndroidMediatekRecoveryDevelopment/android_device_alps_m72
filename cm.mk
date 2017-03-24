7a079f9d18a2bf68e88f153e21768681ec7dd9b7
kirito96 <eagle1968@live.com>
2017-03-24 19:13:17 -0400

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,21 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := m72
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/alps/m72/device_m72.mk)
+
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m72
+PRODUCT_NAME := cm_m72
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := M706
+PRODUCT_MANUFACTURER := alps
